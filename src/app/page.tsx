8f26269b54e92157b7d9c0d009076ae27705256a
Philipp Runge <p.runge@progani.com>
2025-01-18 22:40:29 +0100

    change user list from grid to flex wrap layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,12 @@ export default async function Home() {
 
   return (
     <HydrateClient>
-      <main className="to-primary-0 flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-black text-white">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-black to-primary-0 text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <h1 className="text-center text-5xl font-extrabold tracking-tight sm:text-[5rem]">
             Did You <span className="text-primary-4">Commit</span> Today?
           </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+          <div className="flex flex-wrap justify-center gap-8">
             {users.map((user) => (
               <User key={user.name} user={user} />
             ))}
