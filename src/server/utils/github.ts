0133b498611aed66cd65dba7f1d204b0f8fa6c2b
Philipp Runge <p.runge@progani.com>
2025-01-19 01:15:45 +0100

    screw this, serverless sucks, going back to full octokit mode again

--- src/server/utils/github.ts
+++ src/server/utils/github.ts
@@ -1,94 +1,47 @@
-import chromium from "@sparticuz/chromium-min";
-import puppeteer from "puppeteer";
-import puppeteerCore, { type Page } from "puppeteer-core";
 import { env } from "~/env";
+import { Octokit } from "octokit";
+import { z } from "zod";
 
-async function getElementBySelector(page: Page, selector: string) {
-  try {
-    await page.waitForSelector(selector);
-    return await page.$(selector);
-  } catch (error) {
-    console.error("Error fetching the element:", error);
-  }
+const octokit = new Octokit({
+  auth: env.GITHUB_TOKEN,
+});
+
+const UserSchema = z.object({
+  userName: z.string(),
+  imageUrl: z.string(),
+  profileUrl: z.string(),
+  hasContributedToday: z.boolean(),
+});
+type User = z.infer<typeof UserSchema>;
+async function getUser(username: string) {
+  const { data } = await octokit.rest.users.getByUsername({
+    username,
+  });
+  return {
+    userName: data.login,
+    imageUrl: data.avatar_url,
+    profileUrl: data.html_url,
+    hasContributedToday: false,
+  } satisfies Exclude<User, "hasContributedToday">;
 }
 
-async function getData(username: string) {
-  const profileUrl = `https://github.com/${username}`;
+async function hasUserCommitedToday(username: string) {
+  const { data } = await octokit.rest.activity.listPublicEventsForUser({
+    username,
+  });
 
-  let browser;
+  const todaysEvents = data.filter(
+    (event) =>
+      event.created_at &&
+      new Date(event.created_at).getDate() === new Date().getDate(),
+  );
+  console.log("todaysEvents", todaysEvents);
 
-  if (process.env.NODE_ENV === "development") {
-    console.log("Development browser: ");
-    browser = await puppeteer.launch({
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
-      headless: true,
-    });
-  } else {
-    console.log("Development production: ");
-    browser = await puppeteerCore.launch({
-      args: chromium.args,
-      defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath(),
-      headless: chromium.headless,
-    });
-  }
-  const page = await browser.newPage();
-
-  let imageUrl = "";
-  let hasContributedToday = false;
-  try {
-    // Navigate to the URL
-    await page.goto(profileUrl, { waitUntil: "networkidle2" });
-
-    // Wait for the element to appear (if needed)
-    await Promise.all([
-      page.waitForSelector(".js-calendar-graph-table"),
-      page.waitForSelector(".avatar"),
-    ]);
-
-    // todayLevel
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    const todayLevel = (await page.evaluate((timezone) => {
-      const today = new Date(
-        new Date().getTime() + (timezone - 1) * 60 * 60 * 1000,
-      )
-        .toISOString()
-        .split("T")[0]!;
-
-      // Query the DOM for the relevant elements
-      const table = document.querySelector(".js-calendar-graph-table");
-      if (!table) return null;
-
-      const todayCell = table.querySelector(`td[data-date="${today}"]`);
-      if (!todayCell) return null;
-
-      return todayCell.getAttribute("data-level");
-    }, env.TIMEZONE)) as string;
-    hasContributedToday = parseInt(todayLevel?.toString() ?? "") > 0;
-
-    // imageUrl
-    const avatar = await getElementBySelector(page as Page, ".avatar");
-    if (avatar) {
-      imageUrl =
-        (await avatar.evaluate((img) => {
-          return img?.getAttribute("src");
-        })) ?? "";
-    }
-  } catch (error) {
-    console.error("Error fetching the dynamic element:", error);
-  } finally {
-    // Close the browser
-    await browser.close();
-  }
-
-  return {
-    imageUrl,
-    hasContributedToday,
-  };
+  return todaysEvents.length > 0;
 }
 
 const github = {
-  getData,
+  getUser,
+  hasUserCommitedToday,
 };
 export default github;
